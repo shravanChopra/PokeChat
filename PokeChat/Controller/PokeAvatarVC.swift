9f82b8794c32e77b26eca5ec8f5987c59e4260c1
Shravan Chopra <shrav_20@hotmail.com>
2017-12-26 15:12:39 -0600

    Successfully implemented pokedata loading from CSV

--- PokeChat/Controller/PokeAvatarVC.swift
+++ PokeChat/Controller/PokeAvatarVC.swift
@@ -10,26 +10,50 @@ import UIKit
 
 class PokeAvatarVC: UIViewController {
 
+    // to store pokemon data
+    var pokemon: [Pokemon]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        pokemon = loadPokemonData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+ 
     
+    // Function: loads all pokemon data from CSV file and returns as an array of Pokemon
+    private func loadPokemonData() -> [Pokemon]! {
+        
+        var results = [Pokemon]()
+        
+        guard let filePath = Bundle.main.path(forResource: "pokemon", ofType: "csv")
+            else {
+                return nil
+        }
+        
+        do {
+            let contents = try String(contentsOfFile: filePath, encoding: .utf8)
+            var pokeItems = contents.components(separatedBy: "\r")
+            
+            // removing the last one
+            pokeItems.removeLast()
+            
+            for item in pokeItems {
+                
+                let pokeData = item.components(separatedBy: ",")
+                let newPokemon = Pokemon(id: pokeData[0], name: pokeData[1])
+                results.append(newPokemon)
+            }
+            
+        } catch {
+            print("File Read Error for file \(filePath)")
+            return nil
+        }
+        
+        print ("\(results.count) pokemon loaded")
+        
+        return results
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
