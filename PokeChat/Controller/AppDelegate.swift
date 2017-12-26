e5c02b13ab2ae6a723e550ec7b27e57163c5a9c5
Shravan Chopra <shrav_20@hotmail.com>
2017-12-26 14:13:15 -0600

    Successfully configured Firebase and connected to DB

--- PokeChat/Controller/AppDelegate.swift
+++ PokeChat/Controller/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // configure Firebase
+        FirebaseApp.configure()
+        
+        // TODO: delete this later!
+        let myDB = Database.database().reference()
+        myDB.setValue("Hello from PokeChat!")
+        
         return true
     }
 
