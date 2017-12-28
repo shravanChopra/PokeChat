5675e893744db2d55cfafd64125edcf008a3a41a
Shravan Chopra <shrav_20@hotmail.com>
2017-12-28 15:43:32 -0600

    Implemented animations to adjust textfield height when user begins/ends typing

--- PokeChat/Controller/ChatVC.swift
+++ PokeChat/Controller/ChatVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ChatVC: UIViewController {
+class ChatVC: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
 
     // to store avatar of current user
     private var _currentPokeId: String!
@@ -17,16 +17,85 @@ class ChatVC: UIViewController {
         set { _currentPokeId = newValue }
     }
     
+    // to adjust textfield when the keyboard appears and disappears
+    @IBOutlet weak var newMsgViewHeight: NSLayoutConstraint!
+    
+    // to store and display all messages
+    @IBOutlet weak var messagesTableView: UITableView!
+    
+    // to compose new messages
+    @IBOutlet weak var messageTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        initialSetup()
         
+        // to dismiss keyboard when user taps outside the textfield
+        let tapGesture = UIGestureRecognizer(target: self, action: #selector(tappedOutside))
+        messagesTableView.addGestureRecognizer(tapGesture)
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Function - does initial setup
+    private func initialSetup() {
+        messagesTableView.dataSource = self
+        messagesTableView.delegate = self
+        messagesTableView.separatorStyle = .none
+        
+        messageTextField.delegate = self
     }
+    
+    // ---------------------------------------------------------------------------------------------
+    
+    // MARK - tableView protocol methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
+    }
+    
+    
+    // ---------------------------------------------------------------------------------------------
+    
+    // MARK - textfield delegate methods
+    
+    // Function - increase the height of the view to accommodate the keyboard
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.2) {
+            
+            self.newMsgViewHeight.constant = 318
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    // Function - reset the height of the view once the keyboard has been dismissed
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.2) {
+            self.newMsgViewHeight.constant = 60
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    // Function - dismiss the keyboard when the user hits 'Return'
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Function - dismiss the keyboard once the user taps outside the textfield
+    @objc func tappedOutside() {
+        messageTextField.endEditing(true)
+    }
 }
